25f18c46fa0378edf2de9e12c3ced96c425d62f3
unknown <mksps0406@gmail.com>
2022-04-15 09:38:27 +0530

    firstrepo

---
+++ reverse_string.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWord(string str);
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+string reverseWord(string str){
+    
+  string s="";
+  int n=str.size();
+  for(int i=n-1;i>=0;i--)
+  {
+      s+=str[i];
+  }
+  return s;
+}
